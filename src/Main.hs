1ed987992b592f59d552cdc8bd08d09f4f459af0
mihhail.m <mihhail.matisinets@gmail.com>
2020-10-25 15:49:02 +0200

    the begining

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import System.Environment
+import System.IO
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  withFile (args!!0) ReadMode (\h -> do
+      content <- hGetContents h
+      putStr content)
