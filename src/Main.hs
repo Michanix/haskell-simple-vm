c071c59f333b2733d5fa6d98364fe79c86f658fc
mihhail.m <mihhail.matisinets@gmail.com>
2020-10-25 16:25:16 +0200

    hlint tested

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,6 @@ import System.IO
 main :: IO ()
 main = do
   args <- getArgs
-  withFile (args!!0) ReadMode (\h -> do
+  withFile (head args) ReadMode (\h -> do
       content <- hGetContents h
       putStr content)
