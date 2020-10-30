a0d3467daf7e23c1363b7c0ca2570831a5926243
mihhail.m <mihhail.matisinets@gmail.com>
2020-10-30 11:06:58 +0200

    some progress has been made

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,10 @@ import System.IO
 import Data.Char
 import Data.Data
 import Data.Typeable
+import Control.Monad (forM_)
+import Control.Monad.State
+import Control.Monad.IO.Class (liftIO)
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
@@ -14,7 +18,7 @@ main = do
   case isValidFlag a of
     Just True -> do
       content <- readFile args
-      let s = empty
+      let s = initStack
           xs = map words (lines content)
       print xs
     Nothing   -> error "Wrong flag. Try with -v flag."
@@ -22,73 +26,93 @@ main = do
 type FP = Int
 type PC = Int
 
--- increment PC
-incPC :: PC -> PC
-incPC = (+1)
+-- increment fp and pc
+incC :: Int -> Int
+incC = (+1)
 
-incFP :: FP -> FP
-incFP = (+1)
+incFP' :: State FP FP
+incFP' = do
+  val <- get
+  put (val+1)
+  return val
 
-data VM = VM { stack :: Stack Int
+data VM' = VM' { stack :: Stack
              , fp    :: FP
              , pc    :: PC
              , instr :: Instruction
-             }
+             } deriving (Read)
 
-runInstr :: Instruction -> VM -> VM
--- Loadc v vm = push v (stack vm)
-runInstr (Loadc v) (VM s fp pc insrt) = VM { stack = push v s,
-                             fp = incFP fp,
-                             pc = incPC pc,
-                             instr = Loadc v
-                           }
-runInstr Dup (VM s fp pc instr)      = VM { stack = dup s,
-                             fp = incFP fp,
-                             pc = incPC pc,
-                             instr = Dup
-                           }
-instance Show VM where
+type VM a = StateT VM' IO a
+
+initVM :: VM'
+initVM = VM' {stack=initStack, fp=0, pc=0, instr=None}
+
+
+--loadInstr :: [String] -> VM ()
+loadInstr ::Instruction -> VM ()
+loadInstr (Loadc val) = do
+  vm <- get
+--  maybe (return()) (\x -> modify (push' x)) (Just val)
+  modify (push' val)
+  liftIO $ print vm -- printing intermediate state
+
+runVM :: IO VM'
+--runVM = execStateT (mapM_ (loadInstr) [["loadc", "1"], ["loadc", "2"]]) initVM
+runVM = execStateT (mapM_ (loadInstr) [Loadc 1, Loadc 2]) initVM
+push' :: Int -> VM' -> VM'
+push' x (VM' s fp pc instr) = VM' {stack=(x:s)
+                                  , fp=incC fp
+                                  , pc=incC fp
+                                  , instr=instr
+                                  }
+
+-- loadInstr :: Instruction -> VM VM'
+-- loadInstr i = do
+--   vm <- get
+--   case i of
+--     None      -> return ()
+--     Dup       -> put vm {stack=dup (stack vm),instr=i}
+--     (Loadc v) -> put vm {stack=push v (stack vm),instr=i}
+--   return vm
+
+instance Show VM' where
   show = showVm
 
-showVm :: VM -> String
-showVm (VM {stack = s, fp = fp, pc = pc, instr = instr}) =
+showVm :: VM' -> String
+showVm (VM' {stack = s, fp = fp, pc = pc, instr = instr}) =
     "stack: " ++ (show s)  ++ "\n" ++
     "fp: "    ++ (show fp) ++ "\n" ++
     "pc: "    ++ (show pc) ++ "\n" ++
     "instr: " ++ (show instr)
 
-data Stack a = Stack [a]
-
-instance Show a => Show (Stack a) where
-  show = showStack
-
-showStack :: Show a => Stack a -> String
-showStack (Stack a) = show a -- just show values of stack ie []
+type Stack = [Int]
 
 data Instruction =
-  Loadc Int
+  None
+  | Loadc Int
   | Dup
-  deriving (Typeable, Data) -- need that to show instructions in lowercase
+  deriving (Read, Typeable, Data) -- need that to show instructions in lowercase
 
 instance Show Instruction where
   show  = showInstr
 
 showInstr :: Instruction -> String
 showInstr (Loadc v) = "loadc " ++ (show v)
-showInstr instr = map toLower . showConstr $ toConstr instr
+showInstr instr = showInstr' instr
+  where showInstr' instr = map toLower . showConstr $ toConstr instr
 
-empty :: Stack a
-empty = Stack []
+initStack :: Stack
+initStack = []
 
-dup :: Stack a -> Stack a
-dup (Stack (x:xs)) = Stack (x:x:xs)
+dup :: Stack -> Stack
+dup (x:xs) = x:x:xs
 
-push :: a -> Stack a -> Stack a
-push x (Stack xs) = Stack (x:xs)
+push :: Int -> Stack -> Stack
+push x xs = x:xs
 
-pop :: Stack a -> (Maybe a, Stack a)
-pop (Stack []) = (Nothing, Stack [])
-pop (Stack (x:xs)) = (Just x, Stack xs)
+pop :: Stack -> (Maybe Int, Stack)
+pop [] = (Nothing, [])
+pop (x:xs) = (Just x, xs)
 
 isValidFlag :: String -> Maybe Bool
 isValidFlag "-v" = Just True
